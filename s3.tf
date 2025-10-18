46ce57642a4c198ba1a0a70df59dc7d22c61b8c4
SRITEJA-MOLLETI <sritejm143123@gmail.com>
2025-10-19 00:31:34 +0530

    destory s3 bucket

--- s3.tf
+++ s3.tf
@@ -1,13 +1,13 @@
-resource "random_id" "bucket_id" {
-  byte_length = 4
-}
+# resource "random_id" "bucket_id" {
+#   byte_length = 4
+# }
 
-resource "aws_s3_bucket" "terraform_s3_bucket" {
-  bucket = "terraform-s3-bucket-${random_id.bucket_id.hex}"
-  acl    = "private"
+# resource "aws_s3_bucket" "terraform_s3_bucket" {
+#   bucket = "terraform-s3-bucket-${random_id.bucket_id.hex}"
+#   acl    = "private"
 
-  tags = {
-    creator = "sriteja"
-    project = "terraform"
-  }
-}
+#   tags = {
+#     creator = "sriteja"
+#     project = "terraform"
+#   }
+# }
