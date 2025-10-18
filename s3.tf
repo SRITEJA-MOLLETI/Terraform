6a42286d01248b8ee196ae1b64c047d9fb24fe17
SRITEJA-MOLLETI <sritejm143123@gmail.com>
2025-10-18 20:51:11 +0530

    s3 region correction

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "terraform-s3-bucket" {
   bucket = "terraform-s3-bucket"
-  region = var.aws_region
+  # region = var.aws_region
   tags = {
     creator = "sriteja"
     project = "terraform"
